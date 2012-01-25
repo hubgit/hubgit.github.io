2b2f3a5ad1467d9a15ec0b899c3ea81a6846f385
Alf Eaton <alf.eaton@mendeley.com>
2012-01-25 12:32:29 +0000

    click

--- 2012/01/phone/index.php
+++ 2012/01/phone/index.php
@@ -3,7 +3,10 @@
 <style>body { font-family: Helvetica, Arial, sans-serif; }</style>
 
 <? if ($number): ?>
-<?= htmlspecialchars($number); ?><br><a href="skype:+<?= htmlspecialchars($number); ?>?call">Call With Skype</a>
+<?= htmlspecialchars($number); ?><br><a id="skype" href="skype:+<?= htmlspecialchars($number); ?>?call">Call With Skype</a>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script>$("#skype").click();</script>
 <? else: ?>
 No phone number detected.
 <? endif; ?>
+
