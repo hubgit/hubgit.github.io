faab62fa59e716bd93246d5f02bdf2abe4417021
Alf Eaton <alf@hubmed.org>
2012-01-25 12:45:16 +0000

    Update 2012/01/phone/index.php

--- 2012/01/phone/index.php
+++ 2012/01/phone/index.php
@@ -1,12 +1,21 @@
-<? $number = $_SERVER['x-up-calling-line-id']; ?>
+<? $number = $_SERVER['HTTP_X_UP_CALLING_LINE_ID']; ?>
 
+<meta name = "viewport" content = "width = device-width">
+<meta name = "viewport" content = "initial-scale = 1.0">
 <style>body { font-family: Helvetica, Arial, sans-serif; }</style>
 
 <? if ($number): ?>
-<?= htmlspecialchars($number); ?><br><a id="skype" href="skype:+<?= htmlspecialchars($number); ?>?call">Call With Skype</a>
+
++<?= htmlspecialchars($number); ?>
+<button id="skype" data-tel="<?= htmlspecialchars($number); ?>">Call With Skype</button>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-<script>$("#skype").click();</script>
+<script>
+$("#skype").click(function(){
+ location.href = "skype:+" + $(this).data("tel") + "?call";
+});
+</script>
+
 <? else: ?>
 No phone number detected.
 <? endif; ?>
-
